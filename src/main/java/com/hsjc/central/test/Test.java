38a97cdbb1fc355bec898e830d3b21ab88a1d6e9
Zhang Guoan <zhangga@hsjc.com.cn>
2015-12-30 18:26:59 +0800

    zga:modify syn interface

--- src/main/java/com/hsjc/central/test/Test.java
+++ src/main/java/com/hsjc/central/test/Test.java
@@ -10,5 +10,11 @@ public class Test {
     public static void main(String[] args) {
         String str = MD5Util.encode("NN2x9mwFvF"+ MD5Util.encode("hsjcsso")+"201512212013");
         System.out.println(str);
+
+
+        String validatePwd = MD5Util.encode("NN2x9mwFvF" + MD5Util.encode("hsjcsso") + "201512101052");
+
+        System.out.println(validatePwd);
+
     }
 }
