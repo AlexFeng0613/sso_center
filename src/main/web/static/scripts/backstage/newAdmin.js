45a039ddfceceac44849c79b91b18ab56a2a6421
wuyue <1156216446@qq.com>
2016-03-24 14:26:13 +0800

    wuyue:modify adminList page,addNewAdmin page

--- src/main/web/static/scripts/backstage/newAdmin.js
+++ src/main/web/static/scripts/backstage/newAdmin.js
@@ -7,30 +7,21 @@
  */
 
 $(function(){
+    /**
+     * @author : wuyue
+     * @date : 2016/3/22
+     *
+     * 保存时自动跳转到管理员列表
+     *
+     */
+    $('.selected').click(function(){
+        var userName =$('input[name="userName"]').val();
+        var password =$('input[name="password"]').val();
+        var roleId = $('select[name="roleId"]').val();
+        window.location.href ='/user/adminAddNewAdmin.html?userName=' + userName + '&password=' + password + '&roleId=' + roleId;
+    });
 
-    $('#new_admin').addClass('selected');
 });
-
-/**
- * @author : wuyue
- * @date : 2016/3/22
- *
- * 保存时自动跳转到管理员列表
- *
- */
-
-$('.selected').click(function(){
-
-    var userName =$('input[name="userName"]').val();
-    var password =$('input[name="password"]').val();
-    var roleId = $('select[name="roleId"]').val();
-    console.log(userName);
-    console.log(password);
-    console.log(roleId);
-    window.location.href ='/user/adminAddNewAdmin.html?userName=' + userName + '&password=' + password + '&roleId=' + roleId;
-});
-
-
 
 /**
  * @author : wuyue
