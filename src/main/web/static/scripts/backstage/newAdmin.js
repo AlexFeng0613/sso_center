bdedae97f879ca57e981762839d8f4fabfdc4c78
wuyue <1156216446@qq.com>
2016-03-24 15:38:00 +0800

    wuyue: modify newUser，newAdmin.

--- src/main/web/static/scripts/backstage/newAdmin.js
+++ src/main/web/static/scripts/backstage/newAdmin.js
@@ -5,7 +5,6 @@
  * 新增管理员
  *
  */
-
 $(function(){
     /**
      * @author : wuyue
@@ -18,8 +17,25 @@ $(function(){
         var userName =$('input[name="userName"]').val();
         var password =$('input[name="password"]').val();
         var roleId = $('select[name="roleId"]').val();
+        if(SSOSystem.isEmpty(userName)||SSOSystem.isEmpty(password)){
+            SSOSystem.showAlertDialog("请输入完整信息！");
+            return false;
+        }
         window.location.href ='/user/adminAddNewAdmin.html?userName=' + userName + '&password=' + password + '&roleId=' + roleId;
     });
+
+    /**
+     * @author : wuyue
+     * @date : 2016/3/24
+     *
+     * 取消时自动跳转到管理员列表
+     *
+     */
+    $('.cancel').click(function(){
+        window.location.href ='/page/sso/adminList/1,10,0.html';
+    });
+
+
 
 });
 
--- src/main/web/static/scripts/backstage/newUser.js
+++ src/main/web/static/scripts/backstage/newUser.js
@@ -9,9 +9,38 @@ $(function(){
     /**
      * 新增用户
      */
-    $('.build_submit li').eq(0).click(function(){
+    //$('.build_submit li').eq(0).click(function(){
+    $('.selected').click(function(){
+        var userName =$('input[name="userName"]').val();
+        var realName =$('input[name="realName"]').val();
+        var password =$('input[name="password"]').val();
+        if((SSOSystem.isEmpty(userName)&&SSOSystem.isEmpty(password))
+            ||(SSOSystem.isEmpty(userName)&&SSOSystem.isEmpty(realName))
+            ||(SSOSystem.isEmpty(realName)&&SSOSystem.isEmpty(password))
+            ||(SSOSystem.isEmpty(userName)&&SSOSystem.isEmpty(realName)&&SSOSystem.isEmpty(password))
+        ){
+            SSOSystem.showAlertDialog("请将重要信息填写完整！");
+            return false;
+        }
+        else if(SSOSystem.isEmpty(userName)){
+            SSOSystem.showAlertDialog("用户名不能为空！");
+            return false;
+        }
+        else if(SSOSystem.isEmpty(realName)){
+            SSOSystem.showAlertDialog("真实姓名不能为空！");
+            return false;
+        }
+        else if(SSOSystem.isEmpty(password)){
+            SSOSystem.showAlertDialog("密码不能为空！");
+            return false;
+        }
         $('form').submit();
-    })
+    });
+
+    $('.cancel').click(function(){
+        window.location.href='/page/sso/userList/1,10,0,0,0,0,0.html';
+    });
+
 });
 
 
